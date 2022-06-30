0cd229541e9358e4d41eaf9665e206fbb85df6e7
dolev <dolevizacson@gmail.com>
2022-06-30 18:35:24 +0300

    add blog page

---
+++ src/pages/blog/Blog.page.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import BlogPostList from '../../components/blog-post-list/BlogPostList.component';
+
+const Blog = (): JSX.Element => {
+  return <BlogPostList />;
+};
+
+export default Blog;
