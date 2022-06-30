fedfd6e8166743acf278bba6c58b44b819f7689a
dolev <dolevizacson@gmail.com>
2022-06-30 18:36:10 +0300

    add project page

---
+++ src/pages/projects/Projects.page.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import ProjectsList from '../../components/projects-list/ProjectsList.component';
+
+const Projects = (): JSX.Element => {
+  return <ProjectsList />;
+};
+
+export default Projects;
