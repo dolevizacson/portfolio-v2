4d09633c2ca70789695198520aef278c45df70ff
dolev <dolevizacson@gmail.com>
2022-06-30 18:41:03 +0300

    add app color settings file

---
+++ src/style/app-colors.ts
@@ -0,0 +1,40 @@
+import { AppColor } from '../common/interfaces/app-color.interface';
+
+export const appColor1: AppColor = {
+  color1: 'rgba(17,17,17,1)',
+  color2: 'rgba(255,255,255,1)',
+  color3: 'rgba(5, 5, 5, 1)',
+  color4: 'rgba(30, 30, 30, 1)',
+  color5: 'rgba(17,17,17,1)',
+};
+export const appColor2: AppColor = {
+  color1: 'rgba(232, 232, 232,1)',
+  color2: 'rgba(17,17,17,1)',
+  color3: 'rgba(170, 170, 170,1)',
+  color4: 'rgba(255, 255, 255,1)',
+  color5: 'rgba(232, 232, 232,1)',
+};
+
+export const appColorAnimation1 = {
+  '--color-main-1': 'rgba(17,17,17,1)',
+  '--color-main-2': 'rgba(255,255,255,1)',
+  '--color-main-3': 'rgba(5, 5, 5, 1)',
+  '--color-main-4': 'rgba(45, 45, 45, 1)',
+  '--color-main-5': 'rgba(25,25,25,1)',
+  '--color-font-1': 'rgba(255,255,255,1)',
+  '--color-font-2': 'rgba(17,17,17,1)',
+  '--color-main-11': 'rgba(115, 115, 115, 0.2)',
+  transition: { duration: 0.2 },
+};
+
+export const appColorAnimation2 = {
+  '--color-main-1': 'rgba(232, 232, 232,1)',
+  '--color-main-2': 'rgba(17,17,17,1)',
+  '--color-main-3': 'rgba(170, 170, 170,1)',
+  '--color-main-4': 'rgba(255, 255, 255,1)',
+  '--color-main-5': 'rgba(232, 232, 232,1)',
+  '--color-font-1': 'rgba(17,17,17,1)',
+  '--color-font-2': 'rgba(255,255,255,1)',
+  '--color-main-11': 'rgba(170,170,170,0.3)',
+  transition: { duration: 0.2 },
+};
