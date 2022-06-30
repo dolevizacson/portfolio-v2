e080d73721fe92da4aa71cd9e452d3f6bb618a3e
dolev <dolevizacson@gmail.com>
2022-06-30 18:35:51 +0300

    add home page

---
+++ src/pages/home/Home.page.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import About from '../../components/about/About.component';
+
+const Home = (): JSX.Element => {
+  return (
+    <>
+      <About />
+    </>
+  );
+};
+
+export default Home;
