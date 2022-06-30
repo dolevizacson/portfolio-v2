7a39f00c5fc1a54972fc21e7443f7b202ec29831
dolev <dolevizacson@gmail.com>
2022-06-30 18:36:25 +0300

    add skill page

---
+++ src/pages/skills/Skills.page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import SkillsCategoriesList from '../../components/skills-categories-list/SkillsCategoriesList.component';
+
+const Skills = (): JSX.Element => {
+  
+  return <SkillsCategoriesList />
+  
+};
+
+export default Skills;
