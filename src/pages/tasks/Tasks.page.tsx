307b23633d48e30f61cb3fbc940dc46763934e89
dolev <dolevizacson@gmail.com>
2022-06-30 18:36:38 +0300

    add tasks page

---
+++ src/pages/tasks/Tasks.page.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import TasksList from '../../components/tasks-list/TasksList.component';
+
+const Tasks = (): JSX.Element => {
+  return <TasksList />;
+};
+
+export default Tasks;
