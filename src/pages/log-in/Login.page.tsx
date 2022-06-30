86c763fcaea227a570b496e5f44c0bc4a793aa2b
dolev <dolevizacson@gmail.com>
2022-06-30 18:36:01 +0300

    add login page

---
+++ src/pages/log-in/Login.page.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import LoginForm from '../../components/login-form/LoginForm.component';
+
+const Login = (): JSX.Element => {
+  return <LoginForm />;
+};
+
+export default Login;
